97c5554728ea0ef708978794c28031c6773ef897
Aether-eum <aether@polispay.org>
2021-07-26 17:04:35 -0400

    update

--- src/entities/currency.ts
+++ src/entities/currency.ts
@@ -33,29 +33,23 @@ export class Currency {
 
   public static readonly OKT: Currency = new Currency(18, 'OKT', 'OKExChain')
 
+  public static readonly POLIS: Currency = new Currency(18, 'POLIS', 'Polis')
+
   public static readonly NATIVE = {
     [ChainId.MAINNET]: Currency.ETHER,
-    [ChainId.ROPSTEN]: Currency.ETHER,
     [ChainId.RINKEBY]: Currency.ETHER,
-    [ChainId.GÖRLI]: Currency.ETHER,
-    [ChainId.KOVAN]: Currency.ETHER,
     [ChainId.FANTOM]: Currency.FTM,
-    [ChainId.FANTOM_TESTNET]: Currency.FTM,
     [ChainId.MATIC]: Currency.MATIC,
-    [ChainId.MATIC_TESTNET]: Currency.MATIC,
     [ChainId.XDAI]: Currency.XDAI,
     [ChainId.BSC]: Currency.BNB,
-    [ChainId.BSC_TESTNET]: Currency.BNB,
     [ChainId.ARBITRUM]: Currency.ETHER,
     [ChainId.MOONBASE]: Currency.GLMR,
     [ChainId.AVALANCHE]: Currency.AVAX,
     [ChainId.FUJI]: Currency.AVAX,
     [ChainId.HECO]: Currency.HT,
-    [ChainId.HECO_TESTNET]: Currency.HT,
     [ChainId.HARMONY]: Currency.ONE,
-    [ChainId.HARMONY_TESTNET]: Currency.ONE,
     [ChainId.OKEX]: Currency.OKT,
-    [ChainId.OKEX_TESTNET]: Currency.OKT
+    [ChainId.SPARTA]: Currency.POLIS
   }
 
   /**
